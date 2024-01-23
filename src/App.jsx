44d95adf8eac95dfcc796b979884a2da99b66e7f
Prashant <prashantyadav09783@gmail.com>
2024-01-23 19:07:58 +0530

    routers and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home, Profile, Signin, About, SignUp} from "./pages"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className='bg-black text-white'>Hii check start</h1>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/sign-in' element={<Signin/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/profile' element={<Profile/ >}/>
+    </Routes>
+   </BrowserRouter>
   )
 }
 
