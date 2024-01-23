2b177c10a99a247ffdb4ca24b6eb594a8b7b3f24
Prashant <prashantyadav09783@gmail.com>
2024-01-23 20:05:06 +0530

    mangoDB connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import dotenv from "dotenv"
+import mongoose from "mongoose";
 
-const port = 3000;
+dotenv.congif();
 
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("conneded to mongoDB")
+  })
+  .catch(err => console.log(err)); 
 const app = express();
 app.listen(port, () => {
  console.log(`listing on port ${port}`); 
