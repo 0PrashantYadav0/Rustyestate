3192f44446f61791640a8305f3fbf4e80a27293a
Prashant <prashantyadav09783@gmail.com>
2024-01-27 20:57:42 +0530

    signup and signin

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,18 +1,50 @@
 import { User } from "../models/user.model.js";
 import bcryptjs from "bcryptjs"
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async(req, res, next) => {
 
   try {
     const {username, email, password } = req.body;
+    if(!username && !password && !email) {
+      req.status(403).json({message:"Please enter username, email and password"})
+    }
+
     const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({username, email, password:hashedPassword});
+    const user = await User.create({username, email, password: hashedPassword});
+    const createdUser = await User.findById(user._id).select("-password")
     
-    await newUser.save()
+    
     res.status(201).json("User created successfully")
   
   } catch (error) {
-    next(errorHandler(500, 'error creating user'));
+    next(errorHandler(500, 'Error creating user'));
+  }
+}
+
+export const signin = async(req,res, next) => {
+  const {email, password} = req.body;
+  try {
+    const validUser = await User.findOne({email})
+    if(!validUser){
+      return next(errorHandler(404, 'Email in not correct'))
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if(!validPassword){
+      return next(errorHandler(401, "Password is incorrect"));
+    }
+
+    const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET,)
+
+    const {password: pass, ...rest} = validUser
+
+    res
+    .cookie('access_token', token, {httpOnly: true, expiresIn: '10d' })
+    .status(200)
+    .json(rest)
+  } catch (error) {
+    next(error);
   }
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 function SignUp() {
+
+  const navigate = useNavigate(); 
+
+  const [loading, setLoading] = useState(false);
+  const [err, setErr] = useState("");
+
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData(
+      {
+        ...formData,
+        [e.target.id]: e.target.value,
+      }
+    )
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    try {
+      setErr("")
+      setLoading(true)
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      } )
+      const data = await res.json();
+
+      if(data){
+        navigate('/sign-in');
+      }
+      setErr("")
+    } catch (error) {
+      console.log("error in data input")
+      setErr(error?.message)
+    }finally{
+      setLoading(false);
+    }
+  }
+
+
+
   return (
-    <div>
-      SignUp
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold m-6'>Sign Up</h1>
+      <div>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+          <input type="text" placeholder='username' className='rounded-lg border-2 px-4 py-2' id='username' onChange={handleChange}/>
+          <input type="text" placeholder='email' className='rounded-lg border-2 px-4 py-2' id='email' onChange={handleChange}/>
+          <input type="password" placeholder='password' className='rounded-lg border-2 px-4 py-2' id='password' onChange={handleChange}/>
+          <button disabled={loading} className='bg-slate-700 p-3 text-white rounded-lg hover:bg-slate-800 disabled:opacity-80'>{loading?"Loading...":"Sign Up"}</button>
+        </form>
+        <p className='py-2 text-center text-red-600'>{err}</p>
+        <div className='flex gap-2 mt-5'>
+          <p>Have an account?</p>
+          <Link to={'/sign-in'} className='text-blue-600 '>Sign In</Link>
+        </div>
+      </div>
     </div>
   )
 }
