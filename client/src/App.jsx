4093c17701600cb093e3d4b828caaf22af1b9730
Prashant <prashantyadav09783@gmail.com>
2024-02-18 16:07:26 +0530

    fix bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
+import SignIn from './pages/Signin';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
