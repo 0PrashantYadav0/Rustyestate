884e40d22c433b1c42788521fd20e39f8fa17987
Prashant <prashantyadav09783@gmail.com>
2024-02-18 15:03:38 +0530

    change in authcontroller

--- api/index.js
+++ api/index.js
@@ -1,37 +1,51 @@
 import express from 'express';
-import dotenv from "dotenv"
-import mongoose from "mongoose";
-import userRouter from "./routes/user.routes.js"
-import signupRoute from "./routes/auth.routes.js"
-import connectDB from './db/index.js';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import listingRouter from './routes/listing.routes.js';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
-connectDB()
-.then(() => {
-  app.listen(process.env.PORT || 8000, () => {
-    console.log("Server is listening on port " + process.env.PORT);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB!');
   })
-})
-.catch((err) => {
-  console.log("MongoDB connection error: " + err)
-})
+  .catch((err) => {
+    console.log(err);
+  });
+
+  const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
 
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port 3000!');
+});
 
 app.use('/api/user', userRouter);
-app.use('/api/auth', signupRoute);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'internal error';
+  const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
     success: false,
-    statusCode: statusCode,
-    message: message
+    statusCode,
+    message,
   });
-})
+});
