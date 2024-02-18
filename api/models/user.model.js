d0dea08399bd996b96b1f244fc4d5e025568eb11
Prashant <prashantyadav09783@gmail.com>
2024-02-18 15:04:40 +0530

    changes in user routes and models

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,24 +1,29 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar:{
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  avatar : {
-    type: String,
-    default: "https://imgs.search.brave.com/rCGeCHKkSE_s7ZwRS5VCQTKqCRaHgeLl4QYksqIjGws/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZy/ZWVwaWsuY29tLzI1/Ni8zMTM1LzMxMzU3/MTUucG5n"
-  },
-}, {timestamps: true});
+  { timestamps: true }
+);
 
-export const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+export default User;
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { user, updateUser } from '../controllers/user.controller.js';
-import { verifyToken } from '../utils/verifiedUser.js';
+import { deleteUser, test, updateUser,  getUserListings, getUser} from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
 
 const router = express.Router();
 
-router.get('/user', user);
-router.post('/update/:id',verifyToken, updateUser);
+router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
 export default router;
